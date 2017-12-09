25ec1dabcac951c7912269c5886554dd1119ee4b
lordmanky <graham121@hotmail.com>
2017-12-09 10:16:53 +0000

    Add project files.

---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+
+        }
+
+        [TestCase(-10, 2, -5)]
+        [TestCase(-1, 2, -0.5)]
+        public void TestDivide(double a, double b, double result)
+        {
+            Assert.That(_uut.Divide(a, b), Is.EqualTo(result));
+        }
+    }
+}
